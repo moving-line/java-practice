b6efd2fdea915539ea3cf99a52df2f914845c414
moving-line <movinglinecheck@gmail.com>
2020-04-11 03:50:51 +0900

    요구사항 5 - 인자를 가진 생성자의 인스턴스 생성

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -2,8 +2,11 @@ package next.reflection;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
+import next.optional.User;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,10 +53,26 @@ public class ReflectionTest {
     @Test
     public void privateFieldAccess() throws Exception {
         Class<Student> clazz = Student.class;
-        Student student = clazz.newInstance();
+//        Student student = clazz.newInstance(); deprecated!!
+        Student student = clazz.getDeclaredConstructor().newInstance();
         Field name = clazz.getDeclaredField("name");
         name.setAccessible(true);
         name.set(student, "새이름");
         logger.debug("name:{}, age:{}", student.getName(), student.getAge());
+    }
+
+    @Test
+    public void generateConstructor() throws Exception  {
+        Class<User> clazz = User.class;
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        logger.debug("생성자 갯수 : {}", constructors.length);
+
+        for (Constructor<?> constructor : constructors) {
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            logger.debug("생성자의 파라미터 타입 : {}", (Object) parameterTypes);
+
+            User user = (User)constructor.newInstance("name", 18);
+            logger.debug(user.toString());
+        }
     }
 }
